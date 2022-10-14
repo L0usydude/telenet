1d71025365ff06f9870504073127c2301ab0e741
LousyDude <lousydude@vk.com>
2022-10-14 22:05:15 +0300

    serialize logic

--- src/main/java/com/telenet/telenet/utils/JsonMapper.java
+++ src/main/java/com/telenet/telenet/utils/JsonMapper.java
@@ -1,11 +1,26 @@
 package com.telenet.telenet.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class JsonMapper {
     private static String pathTemplate = "src/main/resources/dataBase/";
-    public static String serialize(Class classType){
-        String dataBasePath = pathTemplate + classType.getSimpleName();
-        return dataBasePath;
+    public static <T> Map<Integer, T> serialize(Class classType) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(pathTemplate + classType.getSimpleName()));
+        String json = reader.readLine();
+        Map <Integer, T> res = new HashMap<>();
+        while(json != null){
+            ObjectMapper objMapper = new ObjectMapper();
+            res.put(objMapper.readValue(json, classType).,objMapper.readValue(json, classType))
+
+            json = reader.readLine();
+        }
+
     }
 }
