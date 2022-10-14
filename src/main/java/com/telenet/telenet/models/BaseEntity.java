f0aef5b6ae963506641b92f83c285e8b0bd4e4a7
slav <vkuznetcov48@gmail.com>
2022-10-15 00:18:20 +0400

    fixed BaseEntity constructor

--- src/main/java/com/telenet/telenet/models/BaseEntity.java
+++ src/main/java/com/telenet/telenet/models/BaseEntity.java
@@ -1,8 +1,10 @@
 package com.telenet.telenet.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @AllArgsConstructor @NoArgsConstructor
 public class BaseEntity {
     protected int id;
 }
