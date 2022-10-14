c808202b77cab33b357931d90cb5cdf05dcca1f2
slav <vkuznetcov48@gmail.com>
2022-10-15 00:18:56 +0400

    class type serializing into json

--- src/main/java/com/telenet/telenet/models/area/Area.java
+++ src/main/java/com/telenet/telenet/models/area/Area.java
@@ -1,9 +1,11 @@
 package com.telenet.telenet.models.area;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.telenet.telenet.models.area.impl.AreaImpl;
 
-@JsonSubTypes(@JsonSubTypes.Type(value = AreaImpl.class, name = "AreaImplJson"))
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "type")
+@JsonSubTypes(@JsonSubTypes.Type(value = AreaImpl.class, name = "AreaImpl"))
 public interface Area {
     void setId(int newId);
     void setName(String newName);
--- src/main/java/com/telenet/telenet/models/area/impl/AreaImpl.java
+++ src/main/java/com/telenet/telenet/models/area/impl/AreaImpl.java
@@ -10,8 +10,15 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor @NoArgsConstructor @Data @Component @JsonTypeName("AreaImplJson") @JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor @NoArgsConstructor @Data @Component @JsonTypeName("AreaImpl") @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AreaImpl extends BaseEntity implements Area {
     private String name;
     private String description;
+
+    public AreaImpl(int id, String name, String description){
+        super(id);
+        this.name = name;
+        this.description = description;
+    }
+
 }
