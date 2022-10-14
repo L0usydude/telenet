3e376529974e7a9ee822aa0e592b634c40bee6f5
slav <vkuznetcov48@gmail.com>
2022-10-14 22:43:29 +0400

    mapper logic

--- src/main/java/com/telenet/telenet/utils/JsonMapper.java
+++ src/main/java/com/telenet/telenet/utils/JsonMapper.java
@@ -3,8 +3,9 @@ package com.telenet.telenet.utils;
 import java.util.Map;
 
 public class JsonMapper {
-    private static String pathTemplate = "dataBase/";
-    public static <T> Map<Integer, T> serialize(){
-
+    private static String pathTemplate = "src/main/resources/dataBase/";
+    public static String serialize(Class classType){
+        String dataBasePath = pathTemplate + classType.getSimpleName();
+        return dataBasePath;
     }
 }
